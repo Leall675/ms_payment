018b27a417a0e60cf10aab180ecad192a4e36387
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-06-24 18:06:38 -0300

    first commit

---
+++ src/main/java/com/desafio/ms_payment/model/Payment.java
@@ -0,0 +1,13 @@
+package com.desafio.ms_payment.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+}
